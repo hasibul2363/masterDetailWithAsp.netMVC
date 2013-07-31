62a35098e7de71477eb98b06970ed6d85e17f269
hasibul2363 <hasibul2363@gmail.com>
2013-07-31 14:52:49 +0600

    Just created New MasterDetail Solution using MVC for one of my blog reader

---
+++ MasterDetailWithAspNetMVC3AndExternalDataSources/MasterDetail/Models/MasterDetailContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+
+namespace MasterDetail.Models
+{
+    public class MasterDetailContext :DbContext
+    {
+        public MasterDetailContext(): base("MasterDetailContext")
+        {
+        }
+
+        public DbSet<SalesMain> SalesMains { get; set; }
+        public DbSet<SalesSub> SalesSubs { get; set; }
+    }
+    
+}
